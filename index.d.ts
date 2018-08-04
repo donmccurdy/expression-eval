64a84ea9526d7779f14587b5dc97bbc8e6d52db7
nodece <nodeces@gmail.com>
2018-08-04 22:40:04 +0800

    Update delcare

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import * as jsep from "jsep";
 
-declare function compile(expression: string | jsep.Expression): any;
-declare function evaluate(node: jsep.Expression, context: object): any;
+declare function compile(expression: string | jsep.Expression): (context?: object) => any;
+declare function evaluate(node: jsep.Expression, context?: object): any;
 
 export { compile, jsep as parse, evaluate as eval };
