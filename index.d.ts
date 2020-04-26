d4cc708b97d0ad6c0e965042283561c5cb7ec420
Don McCurdy <don.r.mccurdy@gmail.com>
2020-04-26 12:53:55 -0700

    Fix type definitions.
    Fixes #34

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,15 @@
 import * as jsep from 'jsep';
 
+declare type operand = number | string;
+declare type unaryCallback = (a: operand) => operand;
+declare type binaryCallback = (a: operand, b: operand) => operand;
+
 declare function compile(expression: string | jsep.Expression): (context: object) => any;
 declare function compileAsync(expression: string | jsep.Expression): (context: object) => Promise<any>;
 declare function evaluate(node: jsep.Expression, context: object): any;
 declare function evaluateAsync(node: jsep.Expression, context: object): Promise<any>;
-declare function addUnaryOp(op: string, (a: any) => any): void;
-declare function addBinaryOp(op: string, (a: any, b: any) => any): void;
+declare function addUnaryOp(op: string, callback: unaryCallback): void;
+declare function addBinaryOp(op: string, callback: binaryCallback): void;
 
 export {
   compile,
